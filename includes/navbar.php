61c05339b21a95bbf4d0efd1b1b094ab90d18af1
Brijesh <biju941997@gmail.com>
2017-08-07 22:47:45 +0530

    Refactored the code

---
+++ includes/navbar.php
@@ -0,0 +1,34 @@
+
+<nav class="navbar navbar-inverse navbar-fixed-top animated flip" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand text-uppercase" href="#">Ricochet CMS</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#" class="text-uppercase">Home</a>
+                </li>
+                <li>
+                    <a href="post.html" class="text-uppercase">Post</a>
+                </li>
+                <li>
+                    <a href="#" class="text-uppercase">About</a>
+                </li>
+                <li>
+                    <a href="#" class="text-uppercase">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
---
+++ includes/sidebar.php
@@ -0,0 +1,83 @@
+
+    <div class="col-md-4">
+
+        <!-- Blog Search Well -->
+        <div class="well">
+            <h4>Blog Search</h4>
+            <div class="input-group" style="margin-top: 15px;">
+                <input type="text" class="form-control">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+            </div>
+            <!-- /.input-group -->
+        </div>
+
+        <!-- <div class="well">
+            <h4>Login</h4>
+            <div class="input-group" style="margin-top: 15px;">
+                <input type="text" name="username" class="form-control">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">
+                        <span class="glyphicon glyphicon-user"></span>
+                    </button>
+                </span>
+            </div>
+            <div class="input-group" style="margin-top: 12px;">
+                <input type="text" name="username" class="form-control">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">
+                        <span class="glyphicon glyphicon-lock"></span>
+                    </button>
+                </span>
+            </div>
+            <div style="margin-top: 15px;">
+                <button class="btn btn-primary">
+                    Login
+                </button>
+            </div>
+        </div> -->
+
+        <!-- Blog Categories Well -->
+        <div class="well">
+            <h4 style="margin-bottom: 25px;">Blog Categories</h4>
+            <div class="row">
+                <div class="col-lg-6">
+                    <ul class="list-unstyled">
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.col-lg-6 -->
+                <div class="col-lg-6">
+                    <ul class="list-unstyled">
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+            <!-- /.row -->
+        </div>
+
+        <!-- Side Widget Well -->
+        <div class="well">
+            <h4>Side Widget Well</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+        </div>
+
+    </div>
