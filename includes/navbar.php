b980984f99c28903d8a74ce3dd79598c9a196ac0
Brijesh <biju941997@gmail.com>
2017-08-13 20:40:10 +0530

    Dynamic retrieval of data form DB for posts

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,5 @@
 
-<nav class="navbar navbar-inverse navbar-fixed-top animated zoomIn" role="navigation">
+<nav class="navbar navbar-inverse navbar-fixed-top animated bounceInDown" role="navigation">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
