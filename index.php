b1cad73b0d7e30c4101d3a88c5460d0975a9f6e1
Brijesh <brijesh.reddy15@siesgst.ac.in>
2017-11-11 15:13:40 +0530

    Added individual post page and link

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
 
                 while ($row = mysqli_fetch_assoc($show_data_from_posts)) {
                     
+                    $post_id        =   $row['post_id'];
                     $post_title     =   $row['post_title'];
                     $post_author    =   $row['post_author'];
                     $post_date      =   $row['post_date'];
@@ -38,7 +39,7 @@
                 <hr>
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author; ?></a>
--- post.php
+++ post.php
@@ -1,100 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Post | Ricochet CMS</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Google Fonts CDN -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto|Ubuntu:400,700" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/blog-post.css" rel="stylesheet">
-
-</head>
-
-<body>
+<?php include "includes/db.php"; ?>
+<?php include "includes/header.php"; ?>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand text-uppercase" href="#">Ricochet CMS</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="index.php" class="text-uppercase">Home</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-uppercase">Post</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-uppercase">About</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-uppercase">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+    <?php include "includes/navbar.php"; ?>
 
     <!-- Page Content -->
     <div class="container">
 
         <div class="row">
 
-            <!-- Blog Post Content Column -->
-            <div class="col-lg-8">
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
 
-                <!-- Blog Post -->
+                <!-- <h1 class="page-header"> -->
+                <h1>
+                    #Our-Blogs
+                    <small> &nbsp;Team Ricochet</small>
+                </h1>
 
-                <!-- Title -->
-                <h1>Blog Post Title</h1>
+            <?php 
 
-                <!-- Author -->
+                if (isset($_GET['p_id'])) {
+                    $caught_id  = $_GET['p_id'];
+                }
+
+                $query = "SELECT * FROM posts WHERE post_id = $caught_id";
+                $show_data_from_posts = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_assoc($show_data_from_posts)) {
+                    
+                    $post_id        =   $row['post_id'];
+                    $post_title     =   $row['post_title'];
+                    $post_author    =   $row['post_author'];
+                    $post_date      =   $row['post_date'];
+                    $post_image     =   $row['post_image'];
+                    $post_content   =   $row['post_content'];
+
+            ?>
+
+                <hr>
+                <hr>
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
+                </h2>
                 <p class="lead">
-                    by <a href="#">Author Name</a>
+                    by <a href="index.php"><?php echo $post_author; ?></a>
                 </p>
-
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
                 <hr>
-
-                <!-- Date/Time -->
-                <p><span class="glyphicon glyphicon-time"></span> Posted on Month Date, at Time</p>
-
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
                 <hr>
+                <p><?php echo $post_content; ?></p><br>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <!-- Preview Image -->
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-
-                <hr>
-
-                <!-- Post Content -->
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
+                <?php } ?>
 
                 <hr>
 
@@ -122,7 +83,7 @@
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
                     <div class="media-body">
-                        <h4 class="media-heading">First comment Brijesh
+                        <h4 class="media-heading">First comment
                             <small>&nbsp; Month Date, at Time</small>
                         </h4>
                         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
@@ -137,7 +98,7 @@
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
                     <div class="media-body">
-                        <h4 class="media-heading">Second comment Uday
+                        <h4 class="media-heading">Second comment
                             <small>&nbsp; Month Date, at Time</small>
                         </h4>
                         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
@@ -147,7 +108,7 @@
                                 <img class="media-object" src="http://placehold.it/64x64" alt="">
                             </a>
                             <div class="media-body">
-                                <h4 class="media-heading">Nested Jasmine
+                                <h4 class="media-heading">Nested comment
                                     <small>&nbsp; Month Date, at Time</small>
                                 </h4>
                                 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
@@ -157,96 +118,27 @@
                     </div>
                 </div>
 
+                <hr>
+
+                <!-- Pager -->
+                <ul class="pager">
+                    <li class="previous">
+                        <a href="#">&larr; Older</a>
+                    </li>
+                    <li class="next">
+                        <a href="#">Newer &rarr;</a>
+                    </li>
+                </ul>
+
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-                <!-- Blog Search Well -->
-                <div class="well">
-                    <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
-                    <!-- /.input-group -->
-                </div>
-
-                <!-- Blog Categories Well -->
-                <div class="well">
-                    <h4 style="margin-bottom: 25px;">Blog Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- /.row -->
-                </div>
-
-                <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
-
-            </div>
+           <?php include "includes/sidebar.php"; ?>
 
         </div>
         <!-- /.row -->
 
         <hr>
 
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <!-- <div class="col-lg-12">
-                    <p>Copyright &copy; Ricochet CMS | 2017<span class="pull-right">Made by Brijesh | (Aspiring Web Developer)</span></p>
-                </div> -->
-                <!-- /.col-lg-12 -->
-                <div class="col-lg-6">
-                    <p>&copy; Ricochet CMS | 2017</p>
-                </div>
-                <div class="col-lg-6">
-                    <p class="pull-right">Developed by Team Ricochet</p>
-                </div>         
-            </div>
-            <!-- /.row -->
-        </footer>
-
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+<!-- Footer -->
+<?php include "includes/footer.php"; ?>
