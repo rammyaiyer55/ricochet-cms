93cd1ff3dfd3b04bad809afd2f4fd6f5009665bf
Brijesh <brijesh.reddy15@siesgst.ac.in>
2018-01-15 20:35:53 +0530

    Select all checkboxes script added

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,1 +1,19 @@
+// TinyMCE WYSIWYG textarea selector script.
 tinymce.init({ selector:'textarea' });
+
+// Select all checkbox script.
+$(document).ready(function () {
+
+	$('#selectAllBoxes').click(function (event) {
+		if(this.checked) {
+			$('.checkboxes').each(function () {
+				this.checked = true;
+			});
+		} else {
+			$('.checkboxes').each(function () {
+				this.checked = false;
+			});
+		}
+	});
+
+});
